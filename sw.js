46b6b191b4fac834b0e0991dd6077d3e0d92cd41
Tobias Toland <github@thinktt.e4ward.com>
2021-04-11 16:58:52 -0500

    fix link to manifest file, add sw.js but non linked yet

---
+++ sw.js
@@ -0,0 +1,30 @@
+self.addEventListener("install", function(event) {
+  event.waitUntil(preLoad());
+});
+
+var preLoad = function(){
+  console.log("Installing web app");
+  return caches.open("offline").then(function(cache) {
+    console.log("caching index and important routes");
+    return cache.addAll(["fonts/", "images/", "yeoldwiz.css", "yeoldwiz.js", "offline.html"]);
+  });
+};
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(checkResponse(event.request).catch(function() {
+    return returnFromCache(event.request);
+  }));
+  event.waitUntil(addToCache(event.request));
+});
+
+var checkResponse = function(request){
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response){
+      if(response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
+      }
+    }, reject);
+  });
+};
