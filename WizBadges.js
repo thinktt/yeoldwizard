44656ef823b7faf9fb4696b7db66fdc62e524d10
Tobias Toland <github@thinktt.e4ward.com>
2023-01-03 19:31:11 -0600

    merge ladder and score badges

--- WizBadges.js
+++ WizBadges.js
@@ -1,88 +1,48 @@
 import { html } from './pageTools.js'
 
-const ladderKing = html`
-  <span v-if="scoreMode == 'ladder'">
-    <span class="badge-box-2">
-      
-      <span v-if="topFeat === 'won'" class="badge-container"> 
-        <span  class="pawn-won" style="color: green" 
-          :title="'Legend tells of a time you beat ' + cmpName">♙</span>
-        <span  class="pawn-head" style="color: green">b</span>
-        <span class="circle"></span>
-      </span>
 
-      <span v-if="topFeat === 'draw'" class="badge-container"> 
-        <span  class="pawn-won" style="color: gray" 
-        :title="'The best you have ever done is draw against ' + cmpName">♙</span>
-        <span  class="pawn-head" style="color: gray">f</span>
-        <span class="circle"></span>
-      </span>
+const template = html`
+  <span class="badge-box" >
 
-      <span v-if="topFeat === 'lost'" class="badge-container"> 
-        <span  class="pawn-won" style="color: crimson" 
-          :title="'You’ve tried but never beaten ' + cmpName">♙</span>
-        <span  class="pawn-head" style="color: crimson">e</span>
-        <span class="circle"></span>
+    <span v-if="topFeat === 'won'" class="badge"> 
+      <span  class="pawn-won" style="color: green" 
+        :title="'Legend tells of a time you beat ' + cmpName">
+          ♙
       </span>
-
     </span>
-</span>
-`
 
-const scoreKing = html`
-  <span v-if="scoreMode == 'score'">
-    <span v-if="score >= 1" class="badge-box" >
-      <img 
-      class="king" 
-      src="images/king-won.png" 
-      alt="postive win score"
-      title="You have a positive score against this opponent"
-      >
+    <span v-if="score > 0" class="badge">
       <span 
         :title="'Your Wiz Score against ' + cmpName + ' is +' + score" class="score">
           +{{score}}
       </span>
-      <span v-if="score >= 2" class="trophy" :title="'You have conqured ' + cmpName">
-        t
-      </span>
-    </span>
+    </span> 
 
-
-    <span v-if="score < 0" class="badge-box score-down">
-      <img 
-      class="king" 
-      src="images/king-lost.png" 
-      alt="negative score"
-      title="You have a negative score against this opponent"
-      >
+    <span v-if="score < 0" class="badge score-down">
       <span 
-        :title="'Your Wiz Score against ' + cmpName + ' is ' + score" class="score">
+        :title="'Your Wiz Score against ' + cmpName + ' is +' + score" class="score">
           {{score}}
       </span>
-      <span v-if="isNemesis" class="badguy" :title="cmpName + ' is your Nemesis'">d</span>
+    </span> 
 
-    </span>
-
-    <span v-if="score === 0" class="badge-box score-even">
-      <img
-      class="king" 
-      src="images/king-draw.png" 
-      alt="even score"
-      title="You have an even score against this opponent"
-      >
+    <span v-if="score === 0" class="badge score-even">
       <span 
         :title="'Your score is even with ' + cmpName" class="score">
           EVEN
       </span>
-    </span> 
-  </span>
-`
+    </span>
 
-const template = html`
-  <span>
-    ${ scoreKing }
-    ${ ladderKing }
-  </span> 
+    <span v-if="isNemesis"> 
+      <span  class="badguy" :title="cmpName + ' is your Nemesis'">d</span>
+    </span> 
+
+    <span v-if="score >= 2" class="badge">
+      <span  class="trophy" :title="'You have conqured ' + cmpName">
+        t
+      </span>
+    </span>
+
+  </span>
 `
 
 export default {
