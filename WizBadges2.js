24a150af6cbe1e6b75a863a7bffb62d63a322e9f
Tobias Toland <github@thinktt.e4ward.com>
2023-01-10 11:38:29 -0600

    fixing the title hints

--- WizBadges2.js
+++ WizBadges2.js
@@ -5,42 +5,47 @@ const template = html`
   <span class="badge-box-2" :class="{'has-selection' : selection}">
 
     <span v-if="topFeat === 'won'" @click="$emit('selectionMade', 'pawn')"
+      :title="'Legend tells of a time you beat ' + cmpName"
       class="badge" :class="{'selected' : selection === 'pawn'}"> 
-        <span  class="pawn-won" :title="'Legend tells of a time you beat ' + cmpName">
+        <span  class="pawn-won">
           ♙
         </span>
     </span>
 
-    <span v-if="score > 0" @click="$emit('selectionMade', 'score')" 
+    <span v-if="score > 0" @click="$emit('selectionMade', 'score')"
+      :title="'Your Wiz Score against ' + cmpName + ' is +' + score" 
       class="badge" :class="{'selected' : selection === 'score'}">
-        <span class="circle" :title="'Your Wiz Score against ' + cmpName + ' is +' + score">
+        <span class="circle">
         </span>
       <span class="score"> +{{score}} </span>
     </span> 
 
     <span v-if="score < 0" @click="$emit('selectionMade', 'score')"
+      :title="'Your Wiz Score against ' + cmpName + ' is ' + score"
       class="badge score-down" :class="{'selected' : selection === 'score'}">
-        <span class="circle" :title="'Your Wiz Score against ' + cmpName + ' is +' + score"> 
+        <span class="circle"> 
         </span>
         <span class="score">{{score}}</span>
     </span> 
 
     <span v-if="score === 0" @click="$emit('selectionMade', 'score')"
+      :title="'Your score is even with ' + cmpName" class="score"
       class="badge score-even" :class="{'selected' :selection === 'score'}">
-        <span 
-          :title="'Your score is even with ' + cmpName" class="score">
+        <span>
             EVEN
         </span>
     </span>
 
     <span v-if="isNemesis" @click="$emit('selectionMade', 'nemesis')"
+      :title="cmpName + ' is your Nemesis'"
       class="badge" :class="{'selected' : selection === 'nemesis'}"> 
         <span  class="badguy" :title="cmpName + ' is your Nemesis'">d</span>
     </span> 
 
-    <span v-if="score >= 2" @click="$emit('selectionMade', 'trophy')" 
+    <span v-if="score >= 2" @click="$emit('selectionMade', 'trophy')"
+      :title="'You have conqured ' + cmpName"
       class="badge" :class="{'selected' : selection === 'trophy'}">
-        <span  class="trophy" :title="'You have conqured ' + cmpName">
+        <span  class="trophy">
           t
         </span>
         <span class="trophy-num">+1</span>
