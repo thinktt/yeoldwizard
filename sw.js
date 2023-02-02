d8a7c0a5bac504999b01e8cf86a4d3bb2039e42b
Tobias Toland <github@thinktt.e4ward.com>
2023-02-02 12:42:25 -0600

    trying to get pwa to work wip

--- sw.js
+++ sw.js
@@ -1,30 +1,81 @@
-self.addEventListener("install", function(event) {
-  event.waitUntil(preLoad());
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-var preLoad = function(){
-  console.log("Installing web app");
-  return caches.open("offline").then(function(cache) {
-    console.log("caching index and important routes");
-    return cache.addAll(["fonts/", "images/", "yeoldwiz.css", "yeoldwiz.js", "offline.html"]);
-  });
-};
-
-self.addEventListener("fetch", function(event) {
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request);
-  }));
-  event.waitUntil(addToCache(event.request));
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage)).catch((err) => {
+        console.log(err)
+      })
+  );
 });
 
-var checkResponse = function(request){
-  return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
-        fulfill(response);
-      } else {
-        reject();
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
       }
-    }, reject);
-  });
-};
+    })());
+  }
+});
+
+// console.log('service worker')
+// self.addEventListener("install", function(event) {
+//   event.waitUntil(preLoad());
+// });
+
+// var preLoad = function(){
+//   console.log("Installing web app");
+//   return caches.open("offline").then(function(cache) {
+//     console.log("caching index and important routes");
+//     return cache.addAll(['offline.html']);
+//   });
+// };
+
+// self.addEventListener("fetch", function(event) {
+//   event.respondWith(checkResponse(event.request).catch(function() {
+//     return returnFromCache(event.request);
+//   }));
+//   event.waitUntil(addToCache(event.request));
+// });
+
+// var checkResponse = function(request){
+//   return new Promise(function(fulfill, reject) {
+//     fetch(request).then(function(response){
+//       if(response.status !== 404) {
+//         fulfill(response);
+//       } else {
+//         reject();
+//       }
+//     }, reject);
+//   });
+// };
