850dc760b4d8215bd88fa87cfc35c27fa8bd35f7
Tobias Toland <github@thinktt.e4ward.com>
2023-05-24 21:40:45 -0500

    use a different b64 function to work on phone

--- signin/king.js
+++ signin/king.js
@@ -5,12 +5,18 @@ const rawKingHashes = [
   '9bd6c1b16251e3a462c61c63b4811605a6072fbeb2311ebe7c05842dd0bfc236',
 ]
 
-
-const kingHashMap = {
+const kingHashMapOld = {
   'f33e751f2b8467193bceee7e480f796b37deeca7259dcc2d420ae395f78de524' : 'D',
   'b2837d3214c7dc0790bc48d25105631bb0aa3c72cc66acffc6b75db964705d2c' : '9',
   '958db8f0e51f3d3648b6a15541c79f0de84765f8829505b19f3100591abd0f41' : '10',
   '40bea41fe03dc9c00a83708bccfbbe45bce28d920eb0fa45900d91c6227f0462' : '11', 
+}
+
+const kingHashMap = {
+  'f33e751f2b8467193bceee7e480f796b37deeca7259dcc2d420ae395f78de524' : 'D',
+  '37af49d12dbe9cc5b7b63229d54ffd6b1861086679bef9575a49ebe4c9040b65' : '9',
+  'd756394d66e30cce156145f74d84b455515a157e8e7803ab7b3632d300dfed17' : '10',
+  '44eba7a5c61a1ebcfc65b03772172a6b7db0cf5ede96ac5161e6b7a351959743' : '11', 
 }
 
 // returns a version number if the blob hash is found in the kingHashMap
@@ -38,13 +44,18 @@ async function getStringHash(str) {
 }
 
 
-async function fileToB64(file) { 
+async function fileToB64Old(file) { 
   return new Promise((resolve, reject) => {
     const reader = new FileReader()
     reader.readAsDataURL(file)
     reader.onload = () => resolve(reader.result)
     reader.onerror = reject
   })
+}
+
+async function fileToB64(file) {
+  const ab = await file.arrayBuffer()
+  return arrayBufferToBase64(ab)
 }
 
 // function that conversts base64 to buffer
@@ -71,6 +82,17 @@ function base64StringToArray(s) {
   return new Uint8Array([...asciiString].map(char => char.charCodeAt(0)))
 }
 
+function arrayBufferToBase64( buffer ) {
+  var binary = '';
+  var bytes = new Uint8Array( buffer );
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+      binary += String.fromCharCode( bytes[ i ] );
+  }
+  return window.btoa( binary );
+}
+
+
 window.king = {
   getVersion,
   rawKingHashes,
@@ -81,6 +103,7 @@ window.king = {
   getStringHash,
   base64StringToArray,
   arrayToBase64String,
+  arrayBufferToBase64,
 }
 
 export default window.king
