f64e6cd6a9f158524400637a1c03c75305e8bcc7
Tobias Toland <github@thinktt.e4ward.com>
2023-06-06 00:54:23 -0500

    displaying and removing animations finally working

--- WizGameEnd.js
+++ WizGameEnd.js
@@ -2,82 +2,84 @@ import { html } from './pageTools.js'
 
 
 const template = html`
-  <div class="slider-box" :class="{noshow: !endMessageIsOn}"> 
-    <div v-if="score === 2 && groupHasTrophy && game.conclusion === 'won'" 
-      class="game-done-message won trophy-win">
-      <wiz-game-status v-if="game" :game="game"></wiz-game-status>
-      <p>
-        Congratulations! You conquered {{groupDisplayName}} by getting every Trophy Point 
-        <span  class="inline-trophy">t</span> <br>
-      </p>
-      <h2  class="you-won">YOU WON</h2>
-      <img class="trophy-piece-end" :class="groupTrophy" :src="trophyImageUrl">
-      <h2 class="color-highlight-gold-1">{{goldenTrophyDisplayMessage}}</h2>
-      <a class="button yellow" @click="hideEndMessage">ok</a>
-    </div>  
+  <Transition name="slide">
+    <div v-if="endMessageIsOn" class="slider-box"> 
+      <div v-if="score === 2 && groupHasTrophy && game.conclusion === 'won'" 
+        class="game-done-message won trophy-win">
+        <wiz-game-status v-if="game" :game="game"></wiz-game-status>
+        <p>
+          Congratulations! You conquered {{groupDisplayName}} by getting every Trophy Point 
+          <span  class="inline-trophy">t</span> <br>
+        </p>
+        <h2  class="you-won">YOU WON</h2>
+        <img class="trophy-piece-end" :class="groupTrophy" :src="trophyImageUrl">
+        <h2 class="color-highlight-gold-1">{{goldenTrophyDisplayMessage}}</h2>
+        <a class="button yellow" @click="hideEndMessage">ok</a>
+      </div>  
 
-    <div v-else class="game-done-message" 
-      :class="{draw: game.conclusion === 'draw', won: game.conclusion === 'won', lost: game.conclusion === 'lost'}">
-      <wiz-game-status v-if="game" :game="game">
-      </wiz-game-status>
-      <p v-if="winCount === 1 && game.conclusion === 'won'">
-      <span class="color-highlight">
-        YOU WON
-      </span>   
-        a pawn badge <span  class="pawn-won">♙</span>
-        for beating {{this.game.opponent}} for the first time
-      </p>
-      <p v-else>
-        <span class="color-highlight-">
-          Your score
+      <div v-else class="game-done-message" 
+        :class="{draw: game.conclusion === 'draw', won: game.conclusion === 'won', lost: game.conclusion === 'lost'}">
+        <wiz-game-status v-if="game" :game="game">
+        </wiz-game-status>
+        <p v-if="winCount === 1 && game.conclusion === 'won'">
+        <span class="color-highlight">
+          YOU WON
         </span>   
-        with {{this.game.opponent}} is now 
-        <span v-if="score === 0" class="inline-score even">EVEN</span>
-        <span v-if="score > 0" class="inline-score up">+{{score}}</span>
-        <span v-if="score < 0" class="inline-score down">{{score}}</span>
-      </p>
+          a pawn badge <span  class="pawn-won">♙</span>
+          for beating {{this.game.opponent}} for the first time
+        </p>
+        <p v-else>
+          <span class="color-highlight-">
+            Your score
+          </span>   
+          with {{this.game.opponent}} is now 
+          <span v-if="score === 0" class="inline-score even">EVEN</span>
+          <span v-if="score > 0" class="inline-score up">+{{score}}</span>
+          <span v-if="score < 0" class="inline-score down">{{score}}</span>
+        </p>
 
-      <p v-if="groupHasTrophy">
-        <span class="color-highlight-green">
-          NEXT GOAL 
-        </span> <br>
-        You've conquered {{groupDisplayName}},  and won the 
-        <span class="color-highlight-gold-1">
-          {{goldenTrophyDisplayMessage}}
-        </span>. Play the next group to collect more golden pieces.
-      </p>
+        <p v-if="groupHasTrophy">
+          <span class="color-highlight-green">
+            NEXT GOAL 
+          </span> <br>
+          You've conquered {{groupDisplayName}},  and won the 
+          <span class="color-highlight-gold-1">
+            {{goldenTrophyDisplayMessage}}
+          </span>. Play the next group to collect more golden pieces.
+        </p>
 
-      <p v-else-if="winCount < 1">
-        <span class="color-highlight-green">
-          NEXT GOAL 
-        </span> <br>    
-        Beat {{this.game.opponent}} to win <br> 
-        The Pawn Badge<span  class="pawn-won">♙</span>
-      </p>
+        <p v-else-if="winCount < 1">
+          <span class="color-highlight-green">
+            NEXT GOAL 
+          </span> <br>    
+          Beat {{this.game.opponent}} to win <br> 
+          The Pawn Badge<span  class="pawn-won">♙</span>
+        </p>
 
-      <p v-else-if="score < 2">
-        <span class="color-highlight-green">
-          NEXT GOAL
-        </span> <br> 
-        Score <span class="inline-score">+2</span>
-        against {{this.game.opponent}} to win a trophy point 
-        <span  class="inline-trophy">t</span>
-      </p>
+        <p v-else-if="score < 2">
+          <span class="color-highlight-green">
+            NEXT GOAL
+          </span> <br> 
+          Score <span class="inline-score">+2</span>
+          against {{this.game.opponent}} to win a trophy point 
+          <span  class="inline-trophy">t</span>
+        </p>
 
-      <p v-else>
-        <span class="color-highlight-green">
-          NEXT GOAL 
-        </span> <br>
-        Win all the trophy points  <span  class="inline-trophy">t</span>
-        for {{groupDisplayName}} to win <br><br> 
-        <span class="color-highlight-gold">
-          {{goldenTrophyDisplayMessage}}
-        </span>
-      </p>
+        <p v-else>
+          <span class="color-highlight-green">
+            NEXT GOAL 
+          </span> <br>
+          Win all the trophy points  <span  class="inline-trophy">t</span>
+          for {{groupDisplayName}} to win <br><br> 
+          <span class="color-highlight-gold">
+            {{goldenTrophyDisplayMessage}}
+          </span>
+        </p>
 
-      <a class="button yellow" @click="hideEndMessage">ok</a>
+        <a class="button yellow" @click="hideEndMessage">ok</a>
+      </div>
     </div>
-  </div>
+  </Transition>
 `
 
 export default {
