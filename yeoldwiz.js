7ab67cd27709ce8c7e0d1cb56b8d094619e5759f
Tobias Toland <github@thinktt.e4ward.com>
2024-11-17 21:02:06 -0600

    remove old stream func, pause for board to load

--- yeoldwiz.js
+++ yeoldwiz.js
@@ -628,7 +628,7 @@ async function startApp(user) {
 
       
         // give some time for the game to start, this is crappy but hopefuly works
-        // await new Promise(resolve => setTimeout(resolve, 3000));
+        // await new Promise(resolve => setTimeout(resolve, 3000))
         
         if (err) {
           console.log('Game did not start')
@@ -661,6 +661,9 @@ async function startApp(user) {
           this.currentGameId = currentGame.id
           this.currentGame = currentGame
           await this.loadBoard(this.currentGame)
+          
+          // give some time to let board load before stream connect
+          await new Promise(resolve => setTimeout(resolve, 250))
           await games.connectGame(this.currentGame, () => {
               this.currentGameId = ''
               this.loadUserGames() 
@@ -668,80 +671,6 @@ async function startApp(user) {
           return
         } 
         this.currentGameId = null
-      },
-      async connectToStream(gameId) {
-        const boardGame = this.currentGame //games.getCurrentLatestGame() || {}
-        console.log(`Attempting to stream ${boardGame.id}`)
-        
-        let resolve
-        const startPromise = new Promise(r => resolve = r)
-        this.currentGame.stream = await lichessApi.getGameStream(boardGame.id, async (event) => {
-          switch(event.type) {
-            // case 'gameFull': 
-            //   console.log(`Succefully connected to Game:`)
-            //   console.log(event.id, event.createdAt, event.state.status) 
-              
-            //   boardGame.moves = event.state.moves ? event.state.moves.split(' ') : []
-            //   if (event.white.id == this.user) { 
-            //     boardGame.playedAs = 'white'
-            //   } else {
-            //     boardGame.playedAs = 'black'
-            //   }
-            //   await this.loadBoard(boardGame)
-            //   this.currentGame.lastEventTime = Date.now()
-            //   resolve()
-            //   break;
-            case 'gameState':
-              this.currentGame.lastEventTime = Date.now()
-              this.currentGame.moves = event.moves.split(' ')
-              // if (this.boardGame.id === this.currentGame.id) {
-              //   this.boardGame.move = this.currentGame.moves
-              // }
-                // this.currentGame = this.boardGame
-              if (event.status === 'aborted') {
-                this.route('back')
-                this.messageType = 'none'
-                this.loadUserGames()
-                return
-              }
-             const endStates = ['mate', 'resign', 'stalemate', 'aborted', 'draw']
-             if (endStates.includes(event.status)) {
-                // console.log(event)
-                console.log('Game ended!')
-                // this.messageType = 'ended'
-                // this.message = `You have completed your game with ${this.selected.name}`
-              
-                // a hacky way to update the board game status in real time
-                // need to create a better state flow from games module
-                if (event.status !== 'aborted') this.boardGame.hasJustEnded = true
-                this.boardGame.status = event.status
-                this.boardGame.lastMoveAt = Date.now()
-                if (event.status === 'draw' || event.status === 'stalemate') { 
-                  this.boardGame.conclusion = 'draw'
-                  this.boardGame.drawType = games.getDrawType(this.boardGame.conclusion, this.boardGame.moves)
-                } else if (this.boardGame.playedAs === event.winner) {
-                  this.boardGame.conclusion = 'won'
-                } else {
-                  this.boardGame.conclusion = 'lost'
-                }
-                
-                this.loadUserGames()
-              }
-              break;
-            // case 'chatLine': 
-            //   if (event.username === 'lichess' && event.text.includes('declines draw') ) {
-            //     this.drawOfferState = 'declined'
-            //   }
-            //   break;
-            // default: 
-            //   console.log('unhandled game event: ' +  event.type)
-            //   console.log(event)
-          } 
-        }, () => {
-          console.log(`game stream ${boardGame.id} has ended`)
-        })
-
-        return startPromise
       },
     },
     watch: {
