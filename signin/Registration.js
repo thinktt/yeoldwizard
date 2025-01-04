16de992905cd20d8d1b7349f364ac818769a1689
Tobias Toland <github@thinktt.e4ward.com>
2025-01-04 14:30:54 -0600

    wip new registartion flow

---
+++ signin/Registration.js
@@ -0,0 +1,14 @@
+import { html } from '../pageTools.js'
+
+
+const template = html`
+  <main class="down front-wall">
+
+  </main>
+`
+
+export default {
+  name: 'WizDisclaimer',
+  template,
+  functional: true,
+}
--- signin/singin.js
+++ signin/singin.js
@@ -11,40 +11,72 @@ const signInLink = await lichessApi.getSignInLink(codeVerifier)
 const template = html`
   <wiz-disclaimer v-if="view === 'disclaimer'" @accept="accept">
   </wiz-disclaimer>
+  
   <main class="down front-wall" :class="{hidden: isHidden}" v-else>
     <h1 class="down-title">Ye Old Wizard</h1>
     <img class="face" src="../images/faces/Dude.png" alt="Wizard">
-    <div v-if="!user">
+    
+    <div v-if="dialog === 'start'">
       <h2 v-if="signInFailed" class="error">
         Failed to connect to Lichess. <br> Please try again. <br>
         If the problem continues conatact your Wiz Admin.
       </h2>
       <h2 v-else> 
-        Welcome to the Ye Old Wizard. <br>
-        Here you can play the Chessmaster bots on your phone or the web. <br>
-        Sign in with Lichess to get started.
+        Welcome to Ye Old Wizard. <br>
+        Here you can play the Chessmaster bots on your phone or the web. 
+        Register a new account to get started or
+        Sign in with Lichess if you already have an account.
       </h2>
-      <a class="button yellow" :href="signInLink">
-        Sign In with Lichess
+      <a class="button yellow" @click="startRegistration">
+        Register a New Account
       </a> <br>
+      <a class="button blue" :href="signInLink">
+        Sign In to Existing Account
+      </a> <br>
+      <!-- <a class="button blue" @click="goToDisclaimer">
+        View Disclaimer
+      </a> <br> -->
+      <!-- <a class="button blue" @click="goToDisclaimer">
+        Install the phone app
+      </a> <br>  -->
       <a class="button blue" @click="allowBotBrowsing">
         Browse the Bots
-      </a> <br>
-      <a class="button blue" @click="goToDisclaimer">
-        View Disclaimer
-      </a>
+      </a> 
     </div>
-    <div  v-else-if="!disclaimerIsAccepted">
+
+    <div v-else-if="dialog === 'noSlots'">
       <h2>
-        Before we get started, please click below to read and agree to the disclaimer.
+        Thanks for your interest in Ye Old Wizard. We are curretnly at capacity 
+        for users. We will open up more slots soon. At that time you will need 
+        The King Chess Engine, and a Lichess account to register. For now feel free
+        to browse the bots. 
       </h2>
+      
+      <a class="button yellow" @click="allowBotBrowsing">
+        Browse the Bots
+      </a> <br>
+      <a class="button blue" :href="signInLink">
+        Sign In to Existing Account
+      </a> <br>
+    </div>
+
+
+    <div v-else-if="dialog === 'openSlots'">
+      <h2>
+        Registration is open! There are {{slots}} slots left. To register
+        you  will need, the King Chess Engine, and a Lichess account. 
+        Accept the disclaimer to get started. 
+      </h2>
+      
       <a class="button blue" @click="goToDisclaimer">
-        View Disclaimer
+        Accept the Disclaimer
       </a>
     </div>
-    <div v-else-if="!engineIsVerified">
+
+          
+    <div v-else-if="dialog === 'engine'">
       <h2>
-        Upload the King Chess Engine from the Chessmaster 9000, 10th Edition, or 11th Edition.
+        Next upload the King Chess Engine from the Chessmaster 9000, 10th Edition, or 11th Edition.
         On your CD, DVD, or install directory look for the file "TheKing.exe" or "TheKing350.exe".
       </h2>
       <a class="button blue" @click="upload">
@@ -56,9 +88,35 @@ const template = html`
       <br>
       <!-- <div class="icon knight spin">♞</div> -->
     </div>
-    <div v-else>
+
+    <div v-else-if="dialog === 'lichess'">
       <h2>
-        You are now reginstered to play games with Ye Old Wizard! 
+         Let's connect to lichess to fisnish your registration. 
+         Ye old Wizard uses Lichess accounts to track your games and intigrate 
+         with lichess tools.
+      </h2>
+      
+      <!-- <a class="button blue" :href="signInLink"> -->
+      <a class="button blue" @click="dialog = 'done'">
+        Sign In to existing account
+      </a> <br>
+      <a class="button yellow"  @click="registerWithLichess">
+        Register new lichess account
+      </a> <br>
+    </div>
+
+    <div v-else-if="dialog === 'redirecting'">
+      <h2>
+        Dicrecting you to lichess registration.<br> 
+        Return to Ye Old Wizard when you have registered
+      </h2>
+      <div class="icon knight spin">♞</div>
+    </div>
+
+    
+    <div v-else-if="dialog === 'done'">
+      <h2>
+        Congradulations you are now registered with Ye Old Wizard! 
         On the next screen you will see all the bots you can play. 
         Select the bot you want to play and then select "Play" 
       </h2>
@@ -66,13 +124,30 @@ const template = html`
         Continue
       </a>
     </div>
+
+    <div v-else-if="dialog === 'install'">
+      <h2>
+        Ye Old Wizard is a Progressive Web App. This means you can install it
+        as a phone app directly from the web. For best experience on a phone
+        install the app. 
+      </h2>
+      <a class="button yellow" :href="rootPath">
+        Install App on Phone
+      </a> <br> 
+      <a class="button blue" :href="rootPath">
+        Skip Installation
+      </a> <br>
+    </div>
+
   </main>
 `
 
 const app = createApp({
   data() {
     return {
+      slots: 0,
       view: 'signIn',
+      dialog: 'start',
       user: localStorage.user,
       engineIsVerified: localStorage.engineIsVerified === 'true',
       disclaimerIsAccepted: localStorage.disclaimerIsAccepted === 'true',
@@ -97,9 +172,27 @@ const app = createApp({
     this.isHidden = false
   },
   methods: {
+    startRegistration() {
+      if (this.slots <= 0) {
+        this.dialog = 'noSlots'
+        return
+      }
+
+      if (true) {}
+
+      
+      this.dialog = 'openSlots'
+    },
+    async registerWithLichess() {
+      this.dialog = 'redirecting'
+      await new Promise(r => setTimeout(r, 1000))
+      window.open('https://lichess.org/signup', '_blank')
+      this.dialog = 'lichess'
+    },
     accept() {
       this.view = 'signIn'
       this.disclaimerIsAccepted = true
+      this.dialog = 'engine'
       localStorage.disclaimerIsAccepted = true
       this.tryMagicWord()
     },
@@ -148,6 +241,9 @@ const app = createApp({
           this.verificationFailed = true
           return
         }
+
+        this.dialog = 'lichess'
+        return 
 
         // upload user info and king blob for backend check and registration
         const id = localStorage.user
